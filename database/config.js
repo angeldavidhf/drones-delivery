0a922d058c285551e6727435017108e2be20b962
Angel D. Franco <angeldavidhf@gmail.com>
2023-07-25 00:15:32 -0500

    :wrench: update database config files and sequelizerc

---
+++ database/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    username: "admin",
+    password: "admin",
+    database: "drone_delivery",
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432,
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'sequelize_data',
+    dialectOptions: {
+        constraint: false,
+    },
+  },
+  test: {
+    username: "admin",
+    password: "admin",
+    database: "drone_delivery",
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432
+  },
+  production: {
+    username: "admin",
+    password: "admin",
+    database: "drone_delivery",
+    host: "localhost",
+    dialect: "postgres",
+    port: 5432
+  }
+};
