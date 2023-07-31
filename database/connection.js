f860ce54d310b2789bbd9e8dd9a758ed3e65fa97
Angel D. Franco <angeldavidhf@gmail.com>
2023-07-31 09:03:16 -0500

    :rocket: Docker files and ajusts

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('drone_delivery_db', 'admin', 'admin', {
     host: 'localhost',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    //logging: console.log
 });
 
 module.exports = sequelize;
